4e34016000d4cb5245793e223ef91323d53c5c69
sunahara <atsushi_sunahara@token.co.jp>
2025-01-29 12:36:41 +0900

    元々修正不要なjs

--- share/js/config.js
+++ share/js/config.js
@@ -1,7 +1,6 @@
 // アプリケーションの設定値
 const CONFIG = {
     // 基本URL設定
-//    BASE_URL: 'https://referrals-flax.vercel.app/index.html',
     BASE_URL: 'https://tohaneda.github.io/saiyo241220.github.io/',
     QR_API_URL: 'https://api.qrserver.com/v1/create-qr-code/',
 
