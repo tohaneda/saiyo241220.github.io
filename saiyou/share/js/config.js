4ac638a8f1f340c924edddf7b2a0c54213a0c1e9
sunahara <atsushi_sunahara@token.co.jp>
2025-02-14 13:58:38 +0900

    move saiyou

--- saiyou/share/js/config.js
+++ saiyou/share/js/config.js
@@ -1,8 +1,8 @@
 // アプリケーションの設定値
 const CONFIG = {
     // 基本URL設定
-//    BASE_URL: 'https://referrals-flax.vercel.app/index.html',
-    BASE_URL: 'https://tohaneda.github.io/saiyo241220.github.io/',
+    // TODO: 本番環境のURLを設定
+    BASE_URL: 'https://tohaneda.github.io/saiyo241220.github.io/saiyou/',
     QR_API_URL: 'https://api.qrserver.com/v1/create-qr-code/',
 
     // API設定
